a828dde9d41fd36823d5b074a01f8c0be5a00ac6
Enoch Phetteplace <curiousaps@gmail.com>
2023-11-16 01:25:22 -0600

    setup new ui

--- app/src/main/java/com/curiousapps/myfoodapp/MainActivity.kt
+++ app/src/main/java/com/curiousapps/myfoodapp/MainActivity.kt
@@ -3,25 +3,70 @@ package com.curiousapps.myfoodapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
-import com.curiousapps.myfoodapp.ui.theme.MyFoodAppTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Column(
+                modifier = Modifier
+                    .background(color = Color(0xFFF2F2F2))
+                    .verticalScroll(rememberScrollState())
+                    .fillMaxHeight()
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.happy_meal_small),
+                    contentDescription = null,
+                    modifier = Modifier.height(300.dp),
+                    contentScale = ContentScale.Crop
+                )
+                Column(
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Text(
+                        text = "Happy meal",
+                        style = TextStyle(
+                            fontSize = 26.sp
+                        )
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Text(
+                        text = "800 calories",
+                        style = TextStyle(
+                            fontSize = 18.sp
+                        )
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Text(
+                        text = "$5.99",
+                        style = TextStyle(
+                            color = Color(0xFF014421),
+                            fontSize = 18.sp
+                        )
+                    )
+                }
+            }
         }
     }
 }
