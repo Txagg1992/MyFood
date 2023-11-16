3232be55a0e9f4997418a3c13b6b8a2a10a8ac95
Enoch Phetteplace <curiousaps@gmail.com>
2023-11-16 00:29:43 -0600

    init commit

---
+++ app/src/main/java/com/curiousapps/myfoodapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.curiousapps.myfoodapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.curiousapps.myfoodapp.ui.theme.MyFoodAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
